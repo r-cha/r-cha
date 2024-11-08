818c67a1d221a62258a9e55f59e5add8b25c790d
Robert Chandler <robert@akchandler.com>
2024-11-08 11:35:40 -0700

    Update design and layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'mono':	['"Oxygen Mono"', ...defaultTheme.fontFamily.mono],
+			}
+		}
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
