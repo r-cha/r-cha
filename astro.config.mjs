6657016f8d42374162e93b8f30d8914a997cc1ec
Robert Chandler <robert@akchandler.com>
2025-04-24 05:23:13 -0700

    Upgrade to tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
 import { transformerNotationDiff } from '@shikijs/transformers';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://r-cha.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   prefetch: true,
   markdown: {
     shikiConfig: {
